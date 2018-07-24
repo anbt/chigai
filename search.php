2b3ba32dc27ace76ff7629c78c0fb75e357b4685
unknown <butan92@gmail.com>
2018-07-24 21:21:29 +0700

    folder display

--- search.php
+++ search.php
@@ -2,15 +2,29 @@
 
 require_once 'initsess.php';
 
-$s = strtolower(trim($_GET['s']));
+$s = trim($_GET['s']);
 $a = &$_SESSION['a'];
 $ret = '';
 $index = 0;
-foreach ($a as $fol => &$b) {
-	foreach ($b as $name) {
-		if (strpos($name['ori'], $s) !== false) {
-			$ret .= '<b>' . sprintf("%02d", ++$index) . '</b>. <a class="change-color" target="_blank" href="' . "chigai/$fol" . '">' . $fol. '</a>/<a class="change-color" target="_blank" href="' . "chigai/$fol/" . $name['enc'] . '">' . $name['ori'] . '</a><br>';
+
+if (substr($s, 0, 1) == '/') { // folder display
+	$s = substr($s, 1);
+	foreach ($a as $fol => &$b) {
+		if (strpos($fol, $s) !== false) {
+			foreach ($b as $name) {
+				$ret .= '<b>' . sprintf("%02d", ++$index) . '</b>. <a class="change-color" target="_blank" href="' . "chigai/$fol" . '">' . $fol. '</a>/<a class="change-color" target="_blank" href="' . "chigai/$fol/" . $name['enc'] . '">' . $name['ori'] . '</a><br>';
+			}
 		}
 	}
 }
+else { // file search
+	foreach ($a as $fol => &$b) {
+		foreach ($b as $name) {
+			if (strpos($name['ori'], $s) !== false) {
+				$ret .= '<b>' . sprintf("%02d", ++$index) . '</b>. <a class="change-color" target="_blank" href="' . "chigai/$fol" . '">' . $fol. '</a>/<a class="change-color" target="_blank" href="' . "chigai/$fol/" . $name['enc'] . '">' . $name['ori'] . '</a><br>';
+			}
+		}
+	}
+}
+
 genOutput($ret);
