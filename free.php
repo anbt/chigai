9487432c97f0b2e41061538ba192d2ac1f5b6cfc
AnBT <butan92@gmail.com>
2018-07-25 08:18:51 +0700

    wrong link 'to index'

--- free.php
+++ free.php
@@ -6,4 +6,4 @@ session_start();
 session_unset(); // $_SESSION = empty
 session_destroy(); // delede session id, obj ...
 echo 'Cleared session in ' . date('Y-m-d H:i:s');
-echo '<br><br>' . buildSessAgain('To index');
+echo '<br><br><a href="index.php">To index</a>';
